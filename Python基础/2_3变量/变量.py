0e92081d482a542cf098b0b1a4295e649f33dd4d
DingJiaxiong <1612102142@qq.com>
2025-12-26 21:31:41 +0800

    2.3 变量

---
+++ Python基础/2_3变量/变量.py
@@ -0,0 +1,23 @@
+Age = 18
+
+age = 15
+
+print(Age)
+print(age)
+
+print(age + 220)
+
+age = 110
+print(age)
+
+age = age + 5
+print(age)
+
+# print(num)
+
+a, b, c = 1, 2, 3
+print(a)
+print(b)
+print(c)
+
+print(type(age))
