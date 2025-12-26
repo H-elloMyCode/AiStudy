f66915eeb18f38805bc0d834bd8445441b22806d
DingJiaxiong <1612102142@qq.com>
2025-12-26 21:16:48 +0800

    2.1 字面量

---
+++ Python基础/1_字面量/字面量.py
@@ -0,0 +1,25 @@
+# 123
+# -45
+# 0
+# 3.14
+# -0.5
+# 2 + 3j
+#
+# 'hello'
+# "world"
+# '''
+# 第一行
+# 第二行
+# '''
+#
+# True
+# False
+# None
+
+print(123)
+print(3.14)
+
+print('python')
+
+print(True)
+print(False)
