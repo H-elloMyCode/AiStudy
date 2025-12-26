f27126d4982d05b66b803dd75c0640aace5901c8
DingJiaxiong <1612102142@qq.com>
2025-12-26 21:40:16 +0800

    2.4.1 数字

---
+++ Python基础/2_4数据类型/2_4_1数字.py
@@ -0,0 +1,34 @@
+age = 25
+print(age)
+print(type(age))
+
+height = 1.75
+print(height)
+print(type(height))
+
+flex = 2+3j
+print(flex)
+print(type(flex))
+
+num = 1.0
+print(num)
+print(type(num))
+
+a = 10
+b = 3
+
+print(a / b)
+print(a % b)
+print(a ** b)
+
+num = 3.9
+print(int(num))
+
+num2 = 5
+print(float(num2))
+
+print(6 / 2)
+
+print(7 // 2)
+
+print(5 + 2.0)
