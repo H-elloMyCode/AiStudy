a87da0db37bc31f31a6e63ea43b4f41748e01149
DingJiaxiong <1612102142@qq.com>
2025-12-27 00:38:39 +0800

    2.5.1 算术运算符

---
+++ Python基础/2_5运算符/2_5_1算术运算符.py
@@ -0,0 +1,32 @@
+a = 10
+b = 3
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+
+print(a // b)
+print(a % b)
+print(a ** b)
+print(4 ** 0.5)
+
+print(-10 // 3)
+print(-10 % 3)
+
+print("===================")
+print('ab' * 3)
+print([1, 2] * 2)
+print('Hello' + 'world')
+
+# print(10 / 0)
+
+num = 18
+if num % 2 == 0:
+    print(f'{num}是偶数')
+
+minutes = 150
+hours = minutes // 60
+mins = minutes % 60
+print(f'{minutes}分钟 = {hours}小时{mins}分钟')
+
+print(25 ** 0.5)
