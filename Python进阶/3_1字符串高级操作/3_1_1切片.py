48ec53933bfebfc09eddf963b93cf60c2f984099
DingJiaxiong <1612102142@qq.com>
2025-12-28 19:07:37 +0800

    3.1.1 切片

---
+++ Python进阶/3_1字符串高级操作/3_1_1切片.py
@@ -0,0 +1,38 @@
+s = 'abcdefgh'
+
+print(s[5:3])
+
+# result = [
+#     s[i:i+2]
+#     for i in range(0, len(s), 2)
+# ]
+
+# print(result)
+
+# def is_palidrome(text):
+#     return text == text[::-1]
+# print(is_palidrome('abcba'))
+# print(is_palidrome('Python'))
+
+
+# s = 'Python教程123'
+#
+#
+#
+# s = '2026Python教程2026'
+# print(s[4:-4])
+
+
+phone = '13812345678'
+# print(phone[-4:])
+
+# print(s[-11])
+# print(s[0:6])
+# print(s[:6])
+# print(s[6:])
+# print(s[-3:])
+# print(s[:-3])
+#
+# print(s[0:20])
+
+#
